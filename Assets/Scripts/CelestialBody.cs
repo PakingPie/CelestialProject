ed8084ea2368741b44100810290538b4389a28cb
PakingPie <john.l.p@live.com>
2025-10-10 14:44:10 +0800

    Minor Updates

--- Assets/Scripts/CelestialBody.cs
+++ Assets/Scripts/CelestialBody.cs
@@ -2,9 +2,11 @@
 using UnityEngine;
 
 [ExecuteInEditMode]
-[RequireComponent (typeof (Rigidbody))]
-public class CelestialBody : GravityObject {
-
+[RequireComponent(typeof(Rigidbody))]
+public class CelestialBody : GravityObject
+{
+    public enum BodyType { Planet, Moon, Sun }
+    public BodyType bodyType;
     public float radius;
     public float surfaceGravity;
     public Vector3 initialVelocity;
@@ -15,15 +17,19 @@ public class CelestialBody : GravityObject {
     public float mass { get; private set; }
     Rigidbody rb;
 
-    void Awake () {
-        rb = GetComponent<Rigidbody> ();
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
         rb.mass = mass;
         velocity = initialVelocity;
     }
 
-    public void UpdateVelocity (CelestialBody[] allBodies, float timeStep) {
-        foreach (var otherBody in allBodies) {
-            if (otherBody != this) {
+    public void UpdateVelocity(CelestialBody[] allBodies, float timeStep)
+    {
+        foreach (var otherBody in allBodies)
+        {
+            if (otherBody != this)
+            {
                 float sqrDst = (otherBody.rb.position - rb.position).sqrMagnitude;
                 Vector3 forceDir = (otherBody.rb.position - rb.position).normalized;
 
@@ -33,30 +39,37 @@ public class CelestialBody : GravityObject {
         }
     }
 
-    public void UpdateVelocity (Vector3 acceleration, float timeStep) {
+    public void UpdateVelocity(Vector3 acceleration, float timeStep)
+    {
         velocity += acceleration * timeStep;
     }
 
-    public void UpdatePosition (float timeStep) {
-        rb.MovePosition (rb.position + velocity * timeStep);
+    public void UpdatePosition(float timeStep)
+    {
+        rb.MovePosition(rb.position + velocity * timeStep);
 
     }
 
-    void OnValidate () {
+    void OnValidate()
+    {
         mass = surfaceGravity * radius * radius / Universe.gravitationalConstant;
-        meshHolder = transform.GetChild (0);
+        meshHolder = transform.GetChild(0);
         meshHolder.localScale = Vector3.one * radius;
         gameObject.name = bodyName;
     }
 
-    public Rigidbody Rigidbody {
-        get {
+    public Rigidbody Rigidbody
+    {
+        get
+        {
             return rb;
         }
     }
 
-    public Vector3 Position {
-        get {
+    public Vector3 Position
+    {
+        get
+        {
             return rb.position;
         }
     }
--- Assets/Scripts/NBodySimulation.cs
+++ Assets/Scripts/NBodySimulation.cs
@@ -2,34 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NBodySimulation : MonoBehaviour {
+public class NBodySimulation : MonoBehaviour
+{
     CelestialBody[] bodies;
     static NBodySimulation instance;
 
-    void Awake () {
+    void Awake()
+    {
 
-        bodies = FindObjectsOfType<CelestialBody> ();
+        bodies = FindObjectsByType<CelestialBody>(FindObjectsSortMode.None);
         Time.fixedDeltaTime = Universe.physicsTimeStep;
-        Debug.Log ("Setting fixedDeltaTime to: " + Universe.physicsTimeStep);
+        Debug.Log("Setting fixedDeltaTime to: " + Universe.physicsTimeStep);
     }
 
-    void FixedUpdate () {
-        for (int i = 0; i < bodies.Length; i++) {
-            Vector3 acceleration = CalculateAcceleration (bodies[i].Position, bodies[i]);
-            bodies[i].UpdateVelocity (acceleration, Universe.physicsTimeStep);
+    void FixedUpdate()
+    {
+        for (int i = 0; i < bodies.Length; i++)
+        {
+            Vector3 acceleration = CalculateAcceleration(bodies[i].Position, bodies[i]);
+            bodies[i].UpdateVelocity(acceleration, Universe.physicsTimeStep);
             //bodies[i].UpdateVelocity (bodies, Universe.physicsTimeStep);
         }
 
-        for (int i = 0; i < bodies.Length; i++) {
-            bodies[i].UpdatePosition (Universe.physicsTimeStep);
+        for (int i = 0; i < bodies.Length; i++)
+        {
+            bodies[i].UpdatePosition(Universe.physicsTimeStep);
         }
 
     }
 
-    public static Vector3 CalculateAcceleration (Vector3 point, CelestialBody ignoreBody = null) {
+    public static Vector3 CalculateAcceleration(Vector3 point, CelestialBody ignoreBody = null)
+    {
         Vector3 acceleration = Vector3.zero;
-        foreach (var body in Instance.bodies) {
-            if (body != ignoreBody) {
+        foreach (var body in Instance.bodies)
+        {
+            if (body != ignoreBody)
+            {
                 float sqrDst = (body.Position - point).sqrMagnitude;
                 Vector3 forceDir = (body.Position - point).normalized;
                 acceleration += forceDir * Universe.gravitationalConstant * body.mass / sqrDst;
@@ -39,16 +47,21 @@ public class NBodySimulation : MonoBehaviour {
         return acceleration;
     }
 
-    public static CelestialBody[] Bodies {
-        get {
+    public static CelestialBody[] Bodies
+    {
+        get
+        {
             return Instance.bodies;
         }
     }
 
-    static NBodySimulation Instance {
-        get {
-            if (instance == null) {
-                instance = FindObjectOfType<NBodySimulation> ();
+    static NBodySimulation Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindAnyObjectByType<NBodySimulation>();
             }
             return instance;
         }
