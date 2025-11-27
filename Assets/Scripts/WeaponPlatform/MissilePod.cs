c49d1594cebcd0941a49a51d83926140a13f1bee
PakingPie <john.l.p@live.com>
2025-11-27 10:07:11 +0800

    Minor Updates

--- Assets/Scripts/WeaponPlatform/MissilePod.cs
+++ Assets/Scripts/WeaponPlatform/MissilePod.cs
@@ -1,14 +1,12 @@
 using UnityEngine;
 
 
-public class MissilePod : MonoBehaviour
+public class MissilePod : WeaponBase
 {
     private AALauncher _launcher;
     private Transform _launchTransform;
 
     public float FireInterval = 2.0f;
-
-    public Transform TargetPoint;
 
     private float _startTime;
 
@@ -22,18 +20,6 @@ public class MissilePod : MonoBehaviour
 
     void Update()
     {
-        if (TargetPoint != null)
-        {
-            // Look at the target.
-            _launchTransform.rotation = Quaternion.LookRotation(TargetPoint.position - transform.position, Vector3.up);
-
-            if (Time.time - _startTime > FireInterval)
-                _launcher.Launch(TargetPoint);
-        }
-        // If no target, just rotate slowly.
-        else
-        {
-            _launchTransform.Rotate(Vector3.up, 10.0f * Time.deltaTime);
-        }
+        
     }
 }
