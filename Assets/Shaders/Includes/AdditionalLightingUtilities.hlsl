de853417d8b1e77add42f37e1ad33435a04b9a88
PakingPie <john.l.p@live.com>
2026-02-24 11:07:30 +0800

    Minor Updates

--- Assets/Shaders/Includes/AdditionalLightingUtilities.hlsl
+++ Assets/Shaders/Includes/AdditionalLightingUtilities.hlsl
@@ -137,4 +137,69 @@ void AdditionalLightsRadianceSimple_float(float MainDiffuse, float3 MainColor, f
     Color = total <= 0 ? MainColor : Color / total;
 #endif
 }
+
+// For CustomLitURP
+void AdditionalLightsURP_float(float MainDiffuse, float3 MainSpecular, float3 MainColor, float3 WorldPosition, float3 WorldNormal, float3 WorldView, float Smoothness, float Reflectance, float2 ScreenPosition, 
+out float Diffuse, out float3 Specular, out float3 Color)
+{
+    Diffuse = MainDiffuse;
+    Specular = MainSpecular;
+    Color = MainColor * (MainDiffuse + MainSpecular);
+
+    #ifndef SHADERGRAPH_PREVIEW
+        
+        uint pixelLightCount = GetAdditionalLightsCount();
+        half Roughness = pow(1 - Smoothness, 2);
+        half Roughness2 = Roughness * Roughness;
+        half Roughness2Minus1 = Roughness2 - 1;
+        half normalizationTerm = (Roughness * half(4.0) + half(2.0));
+
+        #if USE_CLUSTER_LIGHT_LOOP
+            // for Foward+ LIGHT_LOOP_BEGIN macro uses inputData.normalizedScreenSpaceUV and inputData.positionWS
+            InputData inputData = (InputData)0;
+
+            inputData.normalizedScreenSpaceUV = ScreenPosition;
+            inputData.positionWS = WorldPosition;
+        #endif
+
+        LIGHT_LOOP_BEGIN(pixelLightCount)
+            // Convert the pixel light index to the light data index
+            #if !USE_CLUSTER_LIGHT_LOOP
+            lightIndex = GetPerObjectLightIndex(lightIndex);
+            #endif
+            // Call the URP additional light algorithm. This will not calculate shadows, since we don't pass a shadow mask value
+            Light light = GetAdditionalPerObjectLight(lightIndex, WorldPosition);
+            // Manually set the shadow attenuation by calculating realtime shadows
+            light.shadowAttenuation = AdditionalLightRealtimeShadow(lightIndex, WorldPosition, light.direction);
+            #if defined(_LIGHT_COOKIES)
+                float3 cookieColor = SampleAdditionalLightCookie(lightIndex, WorldPosition);
+                light.color *= cookieColor;
+            #endif
+            float NdotL = saturate(dot(WorldNormal, light.direction));
+            float atten = light.distanceAttenuation * light.shadowAttenuation;
+            float thisDiffuse = NdotL * atten;
+            //DirectBRDFSpecular
+
+            float3 lightDirectionWSFloat3 = float3(light.direction);
+            float3 halfDir = SafeNormalize(lightDirectionWSFloat3 + float3(WorldView));
+            float NoH = saturate(dot(float3(WorldNormal), halfDir));
+            half LoH = half(saturate(dot(lightDirectionWSFloat3, halfDir)));
+            float d = NoH * NoH * Roughness2Minus1 + 1.00001f;
+            half LoH2 = LoH * LoH;
+            half spec = Roughness2 / ((d * d) * max(0.1h, LoH2) * normalizationTerm);
+            #if REAL_IS_HALF
+                spec = spec - HALF_MIN;
+                spec = clamp(spec, 0.0, 1000.0);
+            #endif  
+            float3 thisSpecular = spec * Reflectance * NdotL * atten;
+
+            Diffuse += thisDiffuse;
+            Specular += thisSpecular;
+
+            Color += light.color * (thisDiffuse + thisSpecular);
+        LIGHT_LOOP_END
+        float total = Diffuse + dot(Specular, float3(0.333, 0.333, 0.333));
+        Color = total <= 0 ? MainColor : Color / total;
+    #endif
+}
 #endif
