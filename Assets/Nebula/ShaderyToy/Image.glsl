627b4079fa37f63079a9217c4d889e93b870f45c
PakingPie <john.l.p@live.com>
2026-03-06 10:47:08 +0800

    Add Reference

---
+++ Assets/Nebula/ShaderyToy/Image.glsl
@@ -0,0 +1,10 @@
+/*
+    Volumetric nebula rendered in tiles. Use mouse to move camera.
+    Wait for blue noise texture to load.
+*/
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord ){
+    vec2 uv = fragCoord/iResolution.xy;
+    vec3 col = texture(iChannel1, uv).rgb;
+    fragColor = vec4(col, 1.0);
+}
