0b9ee1ea01b019a4a435e354a64243e8ed3666df
PakingPie <john.l.p@live.com>
2026-03-25 09:57:36 +0800

    Minor Updates

--- Assets/Scripts/Boids/BoidsManager.cs
+++ Assets/Scripts/Boids/BoidsManager.cs
@@ -562,10 +562,10 @@ public class BoidsManager : MonoBehaviour
                     var gpuData = request.GetData<BoidData>();
                     
                     // Only copy up to the minimum of GPU buffer size and current _boidData size
-                    int copyCount = Mathf.Min(_readbackBoidCount, _boidData.Length);
+                    int copyCount = Mathf.Min(_readbackBoidCount, Mathf.Min(gpuData.Length, _boidData.Length));
                     if (copyCount > 0)
                     {
-                        System.Array.Copy(gpuData.ToArray(), 0, _boidData, 0, copyCount);
+                        Unity.Collections.NativeArray<BoidData>.Copy(gpuData, 0, _boidData, 0, copyCount);
                     }
                 });
             }
